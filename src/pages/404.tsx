229cf673809e0e2d2866d9b85ec2f20ee0ab2834
jason0814 <jason0814@iabacus.co.kr>
2024-01-02 16:57:27 +0900

    Fix: common style

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="">
+      <h1>404 NotFound.</h1>
+      <div className="box">
+        <h3>hi</h3>
+      </div>
+    </div>
+  )
+}
